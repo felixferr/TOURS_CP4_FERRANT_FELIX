772eb38cef8f7e8b932117bfc48966c0fb82f45e
felix <felixok@live.fr>
2019-07-17 18:03:20 +0200

    app.js

--- wild-circus-api/routes/circus.js
+++ wild-circus-api/routes/circus.js
@@ -69,5 +69,110 @@ router.delete('/actualites/:id', (req, res) => {
         }
     })
 });
+// Consulter les actualites
+router.get('/actualites' , (req, res) => {
+    db.query('SELECT * FROM Actualités', (err, results) => {
+        if(err){
+            res.status(500).send('Erreur lors de la récupération des actualités');
+        } else {
+            res.status(200).json(results);
+        }
+    })
+});
+
+
+//Créer une date 
+router.post('/plannings', (req, res) =>{
+    const formData = req.body;
+    db.query("INSERT INTO Planning SET ?", formData, (err, results) => {
+        if(err){
+            console.log(err)
+            res.status(500).send(`Erreur lors de la création d'un planning`);
+            return;
+        }
+        if (!results) {
+            res.status(400).send();
+            return;
+        } else {
+            db.query("SELECT * FROM Planning WHERE id = ?", results.insertId, (err, results) =>{
+                if (err) {
+                    res.status(500);
+                } else {
+                    res.status(201).send(results[0]);
+                }
+            })
+        }
+    })
+});
+// Supprimer une date du planning
+router.delete('/plannings/:id', (req, res) => {
+    const id = req.params.id;
+    db.query('DELETE FROM Planning WHERE id = ?', id, err => {
+        if(err){
+            console.log(err)
+            res.status(500).send(`Erreur lors de la suppression d'une date du planning`);
+        } else {
+            res.status(204).send();
+        }
+    })
+});
+
+// Consulter les dates du planning
+router.get('/plannings' , (req, res) => {
+    db.query('SELECT * FROM Planning', (err, results) => {
+        if(err){
+            res.status(500).send('Erreur lors de la récupération des plannings');
+        } else {
+            res.status(200).json(results);
+        }
+    })
+});
+
+// Creer un billet
+router.post('/billeteries', (req, res) =>{
+    const formData = req.body;
+    db.query("INSERT INTO billet SET ?", formData, (err, results) => {
+        if(err){
+            console.log(err)
+            res.status(500).send(`Erreur lors de la création d'un billet`);
+            return;
+        }
+        if (!results) {
+            res.status(400).send();
+            return;
+        } else {
+            db.query("SELECT * FROM billet WHERE id = ?", results.insertId, (err, results) =>{
+                if (err) {
+                    res.status(500);
+                } else {
+                    res.status(201).send(results[0]);
+                }
+            })
+        }
+    })
+});
+// Supprimer un billet
+router.delete('/billeteries/:id', (req, res) => {
+    const id = req.params.id;
+    db.query('DELETE FROM billet WHERE id = ?', id, err => {
+        if(err){
+            console.log(err)
+            res.status(500).send(`Erreur lors de la suppression d'un billet`);
+        } else {
+            res.status(204).send();
+        }
+    })
+});
+
+// Consulter les dates du planning
+router.get('/billeteries' , (req, res) => {
+    db.query('SELECT * FROM billet', (err, results) => {
+        if(err){
+            res.status(500).send('Erreur lors de la récupération des billeteries');
+        } else {
+            res.status(200).json(results);
+        }
+    })
+});
 
 module.exports = router;
